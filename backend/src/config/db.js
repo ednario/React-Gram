49c462928973fcc324329c1eeb31aec3ccb2f3ed
Ednario Andrade <ednariocoder@gmail.com>
2023-11-02 20:07:31 -0300

    feat: dbConnection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASS
 const conn = async () => {
   try {
     const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@reactgram.icjhzqf.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://${dbUser}:${dbPassword}@db.50wiyvh.mongodb.net/?retryWrites=true&w=majority`
     )
 
     console.log("Conectou ao banco!");
