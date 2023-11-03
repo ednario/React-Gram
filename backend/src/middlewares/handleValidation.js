df4107e6ac05e4cda8bc0946f39f695e42474fdf
Ednario Andrade <ednariocoder@gmail.com>
2023-11-02 22:01:24 -0300

    feat: ensure middleware handleValidade

---
+++ backend/src/middlewares/handleValidation.js
@@ -0,0 +1,16 @@
+const { validationResult } = require('express-validator')
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return next(), res.status(400).json({ errors: errors.array() })
+  }
+
+  const extractedErrors = []
+
+  errors.array().map(err => extractedErrors.push(err.msg))
+
+  return res.status(422).json({ errors: extractedErrors })
+}
+
+module.exports = validate
