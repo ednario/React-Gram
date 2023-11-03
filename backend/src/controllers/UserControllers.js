9a85f044bf96ee6661170b1667d02cf14d1a6310
Ednario Andrade <ednariocoder@gmail.com>
2023-11-03 18:26:54 -0300

    feat: ensures user registration

--- backend/src/controllers/UserControllers.js
+++ backend/src/controllers/UserControllers.js
@@ -12,10 +12,32 @@ const generateToken = (id) => {
 
 // Register user and sing in
 const registerUser = async (req, res) => {
-  res.send('Register user')
+  const { name, email, password } = req.body
+
+  // check if user exists
+  const user = await User.findOne({ email })
+
+  if(user) {
+    res.status(422).json({ errors: ["email already registered, please use another email address"]})
+    return
+  }
+
+  // Generate password hash
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  try {
+    const user = await newUser.save();
+    res.status(201).json({ user, token: generateToken(user._id) });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
-module.exports = {
-  registerUser,
-  generateToken
-}
+module.exports = {  registerUser  }
