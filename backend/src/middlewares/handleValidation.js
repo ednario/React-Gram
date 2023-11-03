4c5d5a167ce2be578d888dab6665c4c46542812b
Ednario Andrade <ednariocoder@gmail.com>
2023-11-02 22:10:53 -0300

    fix: middleware handleValidation

--- backend/src/middlewares/handleValidation.js
+++ backend/src/middlewares/handleValidation.js
@@ -2,7 +2,7 @@ const { validationResult } = require('express-validator')
 
 const validate = (req, res, next) => {
   const errors = validationResult(req)
-  if (!errors.isEmpty()) {
+  if (errors.isEmpty()) {
     return next(), res.status(400).json({ errors: errors.array() })
   }
 
