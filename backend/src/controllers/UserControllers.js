3afdccdeb2e0ea4fd37bf7a6752e49ded76b9e4d
Ednario Andrade <ednariocoder@gmail.com>
2023-11-09 11:38:30 -0300

    feat: ensures user data is updated

--- backend/src/controllers/UserControllers.js
+++ backend/src/controllers/UserControllers.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -81,7 +82,48 @@ const getCurrentUser = async (req, res) => {
 
 // Update an user
 const updateUser = async (req, res) => {
-  res.send("update user")
+  const { name, password, bio } = req.body
+
+  let profileImage = null
+
+  if(req.file){
+    profileImage = req.file.filename
+  }
+
+  const reqUser = req.user
+
+  const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id))
+
+  if(name) {
+    user.name = name
+  }
+
+  if(password) {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user.password = hashedPassword
+  }
+
+  if(profileImage) {
+    user.profileImage = profileImage
+  }
+
+  if(bio) {
+    user.bio = bio
+  }
+
+  await user.save()
+
+  res.status(200).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    profileImage: user.profileImage,
+    bio: user.bio,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt
+  })
 }
 
 module.exports = {
