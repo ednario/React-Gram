9577e592625d7347d4ddeddc9d08b3e4b7480fa6
Ednario Andrade <ednariocoder@gmail.com>
2024-02-02 11:16:07 -0300

    feat: ensure connection of the registration form to redux

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,25 @@
+import { api, requestConfig } from "../utils/config"
+
+// Register an user
+const register = async (data) => {
+
+  const config = requestConfig("POST", data);
+
+  try {
+    const response = await fetch(`${api}/users/register`, config)
+      .then((response) => response.json())
+      .catch((error) => error);
+
+    if (response) {
+      localStorage.setItem("user", JSON.stringify(response));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const authService = {
+  register
+}
+
+export default authService
